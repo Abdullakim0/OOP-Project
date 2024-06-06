0702e74b9f33f10ed55dbde6afa653e60eab4713
Abdullakim0 <149297845+Abdullakim0@users.noreply.github.com>
2024-06-06 20:16:40 +0200

    Update prob_win_player2.java

--- prob_win_player2.java
+++ prob_win_player2.java
@@ -1,21 +1,40 @@
 import javax.swing.*;
+import java.util.List;
 import java.util.Random;
 public class prob_win_player2 {
-    public String prob_win_player(String round) {
+    public String prob_win_player2(List<String> player1Hand) {
 
-        StringBuilder result=new StringBuilder();
-        result.append("Probability for Player 2: ").append(round).append("\n\n");
+        StringBuilder result = new StringBuilder();
+        result.append("Probability for Player 2: \n\n");
 
+        boolean hasBlackCard = player1Hand.contains("Black");
+        if (hasBlackCard) {
+            result.append("Winning chance is low\n");
+        } else {
+            int high = 0;
+            int avg = 0;
+            int low = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            Random rand = new Random();
-            int randomNum = rand.nextInt(10);
-            if (randomNum < 2) {
-                result.append("Round ").append(i).append(" Your winning chance is low\n");
-            } else if (randomNum <= 4 || randomNum == 2) {
-                result.append("Round ").append(i).append(" Your winning chance is average\n");
+            for (String card : player1Hand) {
+                switch (card) {
+                    case "Red":
+                    case "Blue":
+                        high++;
+                        break;
+                    case "White":
+                        avg++;
+                        break;
+                    case "Black":
+                        low++;
+                        break;
+                }
+            }
+            if (high > avg && high > low) {
+                result.append("Player2 has high winning chance\n");
+            } else if (avg >= high && avg > low) {
+                result.append("player2 has average winning chance\n");
             } else {
-                result.append("Round ").append(i).append(" Your winning chance is high\n");
+                result.append("Player2 has low winning chance\n");
             }
         }
         return result.toString();
